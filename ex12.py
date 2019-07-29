3812b4ec452548bd2dc308dcb2aa4b8b559566ac
Drew Nall <devnall@gmail.com>
2019-07-29 15:40:44 -0400

    Starting fresh

--- ex12.py
+++
@@ -1,5 +0,0 @@
-age1 = input("How old are you? ")
-height1 = input("How tall are you? ")
-weight1 = input("How much do you weigh? ")
-
-print(f"So, you're {age1} old, {height1} tall, and {weight1} heavy.")
